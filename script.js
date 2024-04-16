2172d9eef6587ef78b1c34762387b63d6e15ecec
wnnow <117201813+wnnow@users.noreply.github.com>
2024-04-16 16:51:42 +0700

    Add insert BST in Tree class, mergeSort()

--- script.js
+++ script.js
@@ -25,6 +25,22 @@ class Tree {
     return node;
   }
 
+  insert(key) {
+    this.root = this.insertRec(this.root, key);
+  }
+
+  insertRec(root, key) {
+    if (root == null) {
+      root = new Node(key);
+      return root;
+    }
+
+    if (key < root.data) root.left = this.insertRec(root.left, key);
+    else if (key > root.data) root.right = this.insertRec(root.right, key);
+
+    return root;
+  }
+
   prettyPrint(node, prefix = '', isLeft = true) {
     if (node === null) {
       return;
@@ -51,12 +67,56 @@ function randomArray(number) {
   return tempArr;
 }
 
-const randomArr = randomArray(20);
-console.log('🚀 ~ randomArr:', randomArr);
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    const mid = Math.floor(arr.length / 2);
+    const leftArr = mergeSort(arr.slice(0, mid));
+    const rightArr = mergeSort(arr.slice(mid));
+
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+      if (leftArr[leftIndex] < rightArr[rightIndex]) {
+        result.push(leftArr[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(rightArr[rightIndex]);
+        rightIndex++;
+      }
+    }
+    return result
+      .concat(leftArr.slice(leftIndex))
+      .concat(rightArr.slice(rightIndex));
+  }
+}
+// const randomArr = randomArray(40);
+// const sortedRandomArr = mergeSort(randomArr);
+// console.log('🚀 ~ randomArr:', randomArr);
+// console.log('🚀 ~ sortedRandomArr:', sortedRandomArr);
+
+// const sortedTree = new Tree();
+// sortedTree.root = sortedTree.buildTree(
+//   sortedRandomArr,
+//   0,
+//   sortedRandomArr.length - 1
+// );
+
+// console.log('🚀 ~ sortedTree.root:', sortedTree.root);
+// sortedTree.prettyPrint(sortedTree.root);
 
 const testArray = [1, 2, 3, 4, 5, 6, 7];
-
 const testTree = new Tree();
+
 testTree.root = testTree.buildTree(testArray, 0, testArray.length - 1);
 console.log('🚀 ~ testTree.root:', testTree.root);
+testTree.prettyPrint(testTree.root);
+// testTree.insert(8);
+testTree.insert(8);
+testTree.insert(2);
+testTree.insert(34);
+testTree.insert(13);
 testTree.prettyPrint(testTree.root);
